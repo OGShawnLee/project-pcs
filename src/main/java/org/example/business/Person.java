cf0dd9efd0ddb2496c8d0bd1140fc931d31a347c
OGShawnLee <OGShawnLee@gmail.com>
2025-04-08 18:30:15 -0600

    feat: add StudentDTO

---
+++ src/main/java/org/example/business/Person.java
@@ -0,0 +1,90 @@
+package org.example.business;
+
+import java.time.LocalDateTime;
+
+public abstract class Person {
+  private final String name;
+  private final String paternalLastName;
+  private final String maternalLastName;
+  private final String email;
+  private final String state;
+  private final LocalDateTime createdAt;
+
+  public Person(PBuilder<?> builder) {
+    this.name = builder.name;
+    this.paternalLastName = builder.paternalLastName;
+    this.maternalLastName = builder.maternalLastName;
+    this.email = builder.email;
+    this.state = builder.state;
+    this.createdAt = builder.createdAt;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPaternalLastName() {
+    return paternalLastName;
+  }
+
+  public String getMaternalLastName() {
+    return maternalLastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getState() {
+    return state;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public static abstract class PBuilder<T extends PBuilder<T>> {
+    protected String name;
+    protected String paternalLastName;
+    protected String maternalLastName;
+    protected String email;
+    protected String state;
+    protected LocalDateTime createdAt;
+
+    public T self() {
+      return (T) this;
+    }
+
+    public T setName(String name) {
+      this.name = name;
+      return self();
+    }
+
+    public T setPaternalLastName(String paternalLastName) {
+      this.paternalLastName = paternalLastName;
+      return self();
+    }
+
+    public T setMaternalLastName(String maternalLastName) {
+      this.maternalLastName = maternalLastName;
+      return self();
+    }
+
+    public T setEmail(String email) {
+      this.email = email;
+      return self();
+    }
+
+    public T setState(String state) {
+      this.state = state;
+      return self();
+    }
+
+    public T setCreatedAt(LocalDateTime createdAt) {
+      this.createdAt = createdAt;
+      return self();
+    }
+
+    public abstract Person build();
+  }
+}
---
+++ src/main/java/org/example/business/StudentDTO.java
@@ -0,0 +1,43 @@
+package org.example.business;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentDTO extends Person {
+  private String id;
+
+  public StudentDTO(SBuilder builder) {
+    super(builder);
+    this.id = builder.id;
+  }
+
+  public StudentDTO(ResultSet resultSet) throws SQLException {
+    super(
+      new SBuilder()
+        .setID(resultSet.getString("id_student"))
+        .setEmail(resultSet.getString("email"))
+        .setName(resultSet.getString("name"))
+        .setPaternalLastName(resultSet.getString("paternal_last_name"))
+        .setMaternalLastName(resultSet.getString("maternal_last_name"))
+    );
+    this.id = resultSet.getString("id_student");
+  }
+
+  public String getID() {
+    return id;
+  }
+
+  public static class SBuilder extends PBuilder<SBuilder> {
+    private String id;
+
+    public SBuilder setID(String id) {
+      this.id = id;
+      return this;
+    }
+
+    @Override
+    public StudentDTO build() {
+      return new StudentDTO(this);
+    }
+  }
+}
