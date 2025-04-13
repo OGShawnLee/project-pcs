4078a87340f2d23ba1ec247ad55ce72515958e32
OGShawnLee <OGShawnLee@gmail.com>
2025-04-12 22:16:16 -0600

    feat: add StudentDAO

---
+++ src/main/java/org/example/db/StudentDAO.java
@@ -0,0 +1,110 @@
+package org.example.db;
+
+import org.example.business.StudentDTO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDAO extends DAO<StudentDTO, String> {
+  protected static final String CREATE_QUERY =
+    "INSERT INTO Student (id_student, email, name, paternal_last_name, maternal_last_name) VALUES (?, ?, ?, ?, ?)";
+  private static final String GET_ALL_QUERY =
+    "SELECT * FROM Student";
+  private static final String GET_QUERY =
+    "SELECT * FROM Student WHERE id_student = ?";
+  private static final String UPDATE_QUERY =
+    "UPDATE Student SET name = ?, paternal_last_name = ?, maternal_last_name = ? WHERE id_student = ?";
+  private static final String DELETE_QUERY =
+    "DELETE FROM Student WHERE id_student = ?";
+
+  @Override
+  protected StudentDTO createDTOInstanceFromResultSet(ResultSet resultSet) throws SQLException {
+    return new StudentDTO.StudentBuilder()
+      .setID(resultSet.getString("id_student"))
+      .setEmail(resultSet.getString("email"))
+      .setName(resultSet.getString("name"))
+      .setPaternalLastName(resultSet.getString("paternal_last_name"))
+      .setMaternalLastName(resultSet.getString("maternal_last_name"))
+      .setCreatedAt(resultSet.getTimestamp("created_at").toLocalDateTime())
+      .setState(resultSet.getString("state")).build();
+  }
+
+  @Override
+  public void create(StudentDTO dataObject) throws SQLException {
+    Connection connection = getConnection();
+    PreparedStatement statement = connection.prepareStatement(CREATE_QUERY);
+
+    statement.setString(1, dataObject.getID());
+    statement.setString(2, dataObject.getEmail());
+    statement.setString(3, dataObject.getName());
+    statement.setString(4, dataObject.getPaternalLastName());
+    statement.setString(5, dataObject.getMaternalLastName());
+
+    statement.executeUpdate();
+
+    close();
+  }
+
+  @Override
+  public List<StudentDTO> getAll() throws SQLException {
+    Connection connection = getConnection();
+    PreparedStatement statement = connection.prepareStatement(GET_ALL_QUERY);
+    ResultSet resultSet = statement.executeQuery();
+    ArrayList<StudentDTO> list = new ArrayList<>();
+
+    while (resultSet.next()) {
+      list.add(createDTOInstanceFromResultSet(resultSet));
+    }
+
+    close();
+
+    return list;
+  }
+
+  @Override
+  public StudentDTO get(String filter) throws SQLException {
+    Connection connection = getConnection();
+    PreparedStatement statement = connection.prepareStatement(GET_QUERY);
+    StudentDTO dataObject = null;
+
+    statement.setString(1, filter);
+
+    ResultSet resultSet = statement.executeQuery();
+
+    if (resultSet.next()) {
+      dataObject = createDTOInstanceFromResultSet(resultSet);
+    }
+
+    close();
+
+    return dataObject;
+  }
+
+  @Override
+  public void update(StudentDTO dataObject) throws SQLException {
+    Connection connection = getConnection();
+    PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY);
+
+    statement.setString(1, dataObject.getName());
+    statement.setString(2, dataObject.getPaternalLastName());
+    statement.setString(3, dataObject.getMaternalLastName());
+    statement.setString(4, dataObject.getID());
+
+    statement.executeUpdate();
+    close();
+  }
+
+  @Override
+  public void delete(String id) throws SQLException {
+    Connection connection = getConnection();
+    PreparedStatement statement = connection.prepareStatement(DELETE_QUERY);
+
+    statement.setString(1, id);
+    statement.executeUpdate();
+    close();
+  }
+}
---
+++ src/test/java/db/StudentDAOTest.java
@@ -0,0 +1,125 @@
+package db;
+
+import org.example.business.AccountDTO;
+import org.example.business.StudentDTO;
+import org.example.db.AccountDAO;
+import org.example.db.StudentDAO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentDAOTest {
+  private final StudentDAO STUDENT_DAO = new StudentDAO();
+  private final AccountDAO ACCOUNT_DAO = new AccountDAO();
+  private final String ID = "18014115";
+  private final String EMAIL = "zS18014115@estudiantes.mx";
+  private final String PASSWORD = "Password123";
+  private final String NAME = "Shawn";
+  private final String PATERNAL_LAST_NAME = "Doe";
+  private final String MATERNAL_LAST_NAME = "Lee";
+  private final StudentDTO BASE_STUDENT_DTO = new StudentDTO.StudentBuilder()
+    .setID(ID)
+    .setEmail(EMAIL)
+    .setName(NAME)
+    .setPaternalLastName(PATERNAL_LAST_NAME)
+    .setMaternalLastName(MATERNAL_LAST_NAME)
+    .build();
+  private final AccountDTO BASE_ACCOUNT_DTO = new AccountDTO(EMAIL, PASSWORD);
+
+  private void createTestData() throws SQLException {
+    ACCOUNT_DAO.create(BASE_ACCOUNT_DTO);
+    STUDENT_DAO.create(BASE_STUDENT_DTO);
+  }
+
+  @AfterEach
+  public void tearDown() throws SQLException {
+    STUDENT_DAO.delete(ID);
+    ACCOUNT_DAO.delete(EMAIL);
+  }
+
+  @Test
+  public void testCreateStudent() {
+    assertDoesNotThrow(() -> {
+      createTestData();
+
+      StudentDTO createdStudent = STUDENT_DAO.get(ID);
+
+      Assertions.assertNotNull(createdStudent);
+      Assertions.assertEquals(ID, createdStudent.getID());
+      Assertions.assertEquals(EMAIL, createdStudent.getEmail());
+      Assertions.assertEquals(NAME, createdStudent.getName());
+      Assertions.assertEquals(PATERNAL_LAST_NAME, createdStudent.getPaternalLastName());
+      Assertions.assertEquals(MATERNAL_LAST_NAME, createdStudent.getMaternalLastName());
+    });
+  }
+
+  @Test
+  public void testGetAllStudents() {
+    assertDoesNotThrow(() -> {
+      createTestData();
+
+      List<StudentDTO> studentList = STUDENT_DAO.getAll();
+
+      Assertions.assertNotNull(studentList);
+      Assertions.assertFalse(studentList.isEmpty());
+    });
+  }
+
+  @Test
+  public void testGetStudent() {
+    assertDoesNotThrow(() -> {
+      ACCOUNT_DAO.create(BASE_ACCOUNT_DTO);
+      STUDENT_DAO.create(BASE_STUDENT_DTO);
+
+      StudentDTO student = STUDENT_DAO.get(ID);
+
+      Assertions.assertNotNull(student);
+    });
+  }
+
+  @Test
+  public void testUpdateAcademic() {
+    assertDoesNotThrow(() -> {
+      createTestData();
+
+      String updatedName = "John Halo";
+      String updatedMaternalLastName = "Smithson";
+      String updatedPaternalLastName = "Does";
+
+      StudentDTO updatedStudent = new StudentDTO.StudentBuilder()
+        .setID(ID)
+        .setEmail(EMAIL)
+        .setName(updatedName)
+        .setPaternalLastName(updatedPaternalLastName)
+        .setMaternalLastName(updatedMaternalLastName)
+        .build();
+
+      STUDENT_DAO.update(updatedStudent);
+
+      StudentDTO retrievedStudent = STUDENT_DAO.get(ID);
+
+      Assertions.assertEquals(updatedMaternalLastName, retrievedStudent.getMaternalLastName());
+      Assertions.assertEquals(updatedPaternalLastName, retrievedStudent.getPaternalLastName());
+      Assertions.assertEquals(updatedName, retrievedStudent.getName());
+    });
+  }
+
+  @Test
+  public void testDeleteAcademic() {
+    assertDoesNotThrow(() -> {
+      createTestData();
+
+      ACCOUNT_DAO.delete(EMAIL);
+      STUDENT_DAO.delete(ID);
+
+      StudentDTO deleteStudent = STUDENT_DAO.get(ID);
+
+      Assertions.assertNull(deleteStudent);
+    });
+  }
+}
