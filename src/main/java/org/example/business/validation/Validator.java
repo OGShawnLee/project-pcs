6b65226003f37e87db5acb37c9053ac4118a292a
Chema54 <josemariacm005@gmail.com>
2025-04-28 18:40:53 -0600

    fix: add more validations and fix validations

--- src/main/java/org/example/business/validation/Validator.java
+++ src/main/java/org/example/business/validation/Validator.java
@@ -5,16 +5,37 @@ import org.example.business.Result;
 public class Validator {
   private static final String EMAIL_REGEX = "^[\\w-.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
 
-  public static Result<String> getFilledString(String value, String message, int maxLength) {
-    if (value == null || value.trim().isEmpty() || value.trim().length() > maxLength) {
+  public static Result<String> getFilledString(String value, String message) {
+    if (value == null ||value.trim().isEmpty()) {
       return Result.createFailureResult(message);
     }
-
     return Result.createSuccessResult(value);
   }
 
-  public static Result<String> getEmail(String value, String message, int maxLength) {
-    Result<String> emailResult = getFilledString(value, message, maxLength);
+  public static Result<String> getMaxLenght(String value, String message, int maxLength) {
+    if (value.trim().length() > maxLength) {
+      return Result.createFailureResult(message);
+    }
+    return Result.createSuccessResult(value);
+  }
+
+  public static Result<String> getWords(String value, String message, int maxLength) {
+    if (value == null || !value.matches(".*[^a-zA-Z].*")) {
+      return Result.createFailureResult(message);
+    }
+    return Result.createSuccessResult(value);
+  }
+
+  public static Result<String> getIDStudent(String value, String message, int maxLength) {
+    if (!value.matches("[0-9]+") || value.trim().length() != maxLength) {
+      return Result.createFailureResult(message);
+    }
+    return Result.createSuccessResult(value);
+  }
+
+
+  public static Result<String> getEmail(String value, String message) {
+    Result<String> emailResult = getFilledString(value, message);
 
     if (emailResult.isFailure()) {
       return emailResult;
