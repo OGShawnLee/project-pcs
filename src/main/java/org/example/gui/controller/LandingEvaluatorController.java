76a7234453287892c9d6d47835fc0d96fb9ca1e9
Chema54 <josemariacm005@gmail.com>
2025-06-20 16:42:00 -0600

    fix: add register evaluation use case

--- src/main/java/org/example/gui/controller/LandingEvaluatorController.java
+++ src/main/java/org/example/gui/controller/LandingEvaluatorController.java
@@ -1,6 +1,10 @@
 package org.example.gui.controller;
 
+import org.example.business.dao.Configuration;
+import org.example.business.dao.ConfigurationDAO;
 import org.example.business.dao.PracticeDAO;
+import org.example.business.dto.ConfigurationDTO;
+import org.example.business.dto.EvaluationDTO;
 import org.example.business.dto.PracticeDTO;
 import org.example.gui.Modal;
 
@@ -9,7 +13,7 @@ import java.util.List;
 
 public class LandingEvaluatorController extends LandingController {
   public void navigateToReviewEvaluationListPage() {
-    Modal.display("Registrar Solicitud de Proyecto", "RegisterProjectRequestPAge");
+    Modal.display("Lista de Evaluaciones", "ReviewEvaluationListPage");
   }
 
   public void handleOpenRegisterEvaluation() {
@@ -21,7 +25,20 @@ public class LandingEvaluatorController extends LandingController {
         return;
       }
 
-      Modal.display("Registrar Evaluacion", "RegisterEvaluationModal");
+      List<ConfigurationDTO> currentConfiguration = new ConfigurationDAO().getAll();
+
+      for (ConfigurationDTO config : currentConfiguration) {
+        String configName = String.valueOf(config.name());
+        if (configName.equals(Configuration.EVALUATION_ENABLED_FIRST.name()) && config.isEnabled()) {
+          Modal.display("Registrar Evaluacion-1er Parcial", "RegisterEvaluationModal");
+        } else if (configName.equals(Configuration.EVALUATION_ENABLED_SECOND.name()) && config.isEnabled()) {
+          Modal.display("Registrar Evaluacion-2do Parcial", "RegisterEvaluationModal");
+        } else if (configName.equals(Configuration.EVALUATION_ENABLED_FINAL.name()) && config.isEnabled()) {
+          Modal.display("Registrar Evaluacion-Final", "RegisterEvaluationModal");
+        }
+      }
+      Modal.displayError("No se han habilitado las evaluaciones");
+
     } catch (SQLException e) {
       Modal.displayError("No fue posible recuperar las practicas profesionales");
     }
--- src/main/java/org/example/gui/controller/RegisterEvaluationController.java
+++ src/main/java/org/example/gui/controller/RegisterEvaluationController.java
@@ -6,21 +6,23 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import org.example.business.auth.AuthClient;
-import org.example.business.dao.AcademicDAO;
-import org.example.business.dao.PracticeDAO;
+import org.example.business.dao.*;
 import org.example.business.dao.filter.FilterEvaluation;
-import org.example.business.dto.*;
-import org.example.business.dao.EvaluationDAO;
+import org.example.business.dto.AcademicDTO;
+import org.example.business.dto.ConfigurationDTO;
+import org.example.business.dto.EvaluationDTO;
+import org.example.business.dto.PracticeDTO;
 import org.example.gui.Modal;
 
 import java.sql.SQLException;
 import java.util.List;
 
-public class RegisterEvaluationController extends Controller{
+public class RegisterEvaluationController extends Controller {
 
     private static final AcademicDAO ACADEMIC_DAO = new AcademicDAO();
     private static final EvaluationDAO EVALUATION_DAO = new EvaluationDAO();
-    private static final PracticeDAO PRACTICE_DTO = new PracticeDAO();
+    private static final PracticeDAO PRACTICE_DAO = new PracticeDAO();
+    private static final ConfigurationDAO CONFIGURATION_DAO = new ConfigurationDAO();
 
     @FXML
     private ComboBox<PracticeDTO> comboBoxPractice;
@@ -52,26 +54,51 @@ public class RegisterEvaluationController extends Controller{
         comboBoxRequirementsGrade.setItems(grades);
     }
 
-
     public void loadComboBoxEnrollment() {
         try {
-            List<PracticeDTO> enrollmentList = PRACTICE_DTO.getAll();
+            List<PracticeDTO> practiceList = PRACTICE_DAO.getAll();
 
-            if (enrollmentList.isEmpty()) {
-                Modal.displayError("No existe una organización. Por favor, registre una organización antes de registrar un proyecto.");
+            if (practiceList.isEmpty()) {
+                Modal.displayError("No existe una práctica. Por favor, registre una práctica antes de registrar una evaluación.");
                 return;
             }
 
-            comboBoxPractice.getItems().addAll(enrollmentList);
-            comboBoxPractice.setValue(enrollmentList.getFirst());
+            comboBoxPractice.getItems().addAll(practiceList);
+            comboBoxPractice.setValue(practiceList.getFirst());
         } catch (SQLException e) {
-            Modal.displayError("No ha sido posible cargar las organizaciones debido a un error en el sistema.");
+            Modal.displayError("No ha sido posible cargar las prácticas debido a un error en el sistema.");
         }
     }
 
     public void handleRegister() {
         try {
+            if (comboBoxPractice.getValue() == null || comboBoxSkillGrade.getValue() == null ||
+                    comboBoxContentGrade.getValue() == null || comboBoxWritingGrade.getValue() == null ||
+                    comboBoxRequirementsGrade.getValue() == null || textAreaFeedback.getText().isBlank()) {
+                Modal.displayError("Todos los campos deben estar completos.");
+                return;
+            }
+
             AcademicDTO currentAcademic = ACADEMIC_DAO.getOneByEmail(AuthClient.getInstance().getCurrentUser().email());
+
+            List<ConfigurationDTO> currentConfiguration = CONFIGURATION_DAO.getAll();
+            EvaluationDTO.Kind period = null;
+
+            for (ConfigurationDTO config : currentConfiguration) {
+                String configName = String.valueOf(config.name());
+                if (configName.equals(Configuration.EVALUATION_ENABLED_FIRST.name()) && config.isEnabled()) {
+                    period = EvaluationDTO.Kind.FIRST_PERIOD;
+                    break;
+                } else if (configName.equals(Configuration.EVALUATION_ENABLED_SECOND.name()) && config.isEnabled()) {
+                    period = EvaluationDTO.Kind.SECOND_PERIOD;
+                    break;
+                } else if (configName.equals(Configuration.EVALUATION_ENABLED_FINAL.name()) && config.isEnabled()) {
+                    period = EvaluationDTO.Kind.FINAL;
+                    break;
+                }
+            }
+
+
             EvaluationDTO evaluationDTO = new EvaluationDTO.EvaluationBuilder()
                     .setIDProject(comboBoxPractice.getValue().getIDProject())
                     .setIDStudent(comboBoxPractice.getValue().getIDStudent())
@@ -79,8 +106,9 @@ public class RegisterEvaluationController extends Controller{
                     .setContentCongruenceGrade(String.valueOf(comboBoxContentGrade.getValue()))
                     .setMethodologicalRigorGrade(String.valueOf(comboBoxRequirementsGrade.getValue()))
                     .setWritingGrade(String.valueOf(comboBoxWritingGrade.getValue()))
-                    .setAdequateUseGrade(String.valueOf(comboBoxSkillGrade))
+                    .setAdequateUseGrade(String.valueOf(comboBoxSkillGrade.getValue()))
                     .setFeedback(textAreaFeedback.getText())
+                    .setKind(period)
                     .build();
 
             FilterEvaluation filterEvaluation = new FilterEvaluation(
@@ -91,18 +119,17 @@ public class RegisterEvaluationController extends Controller{
 
             EvaluationDTO existingEvaluationDTO = EVALUATION_DAO.getOne(filterEvaluation);
             if (existingEvaluationDTO != null) {
-                Modal.displayError("No ha sido posible registrar académico debido a que ya existe un académico con la misma ID de Trabajador.");
+                Modal.displayError("Ya existe una evaluación registrada para esta práctica.");
                 return;
             }
 
             EVALUATION_DAO.createOne(evaluationDTO);
-            Modal.displaySuccess("El académico ha sido registrado exitosamente.");
+            Modal.displaySuccess("La evaluación ha sido registrada exitosamente.");
         } catch (IllegalArgumentException e) {
             Modal.displayError(e.getMessage());
         } catch (SQLException e) {
             System.out.println(e.getMessage());
-            Modal.displayError("No ha sido posible registrar académico debido a un error en el sistema.");
+            Modal.displayError("No ha sido posible registrar la evaluación debido a un error en el sistema.");
         }
     }
-
 }
